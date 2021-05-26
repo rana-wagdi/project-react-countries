4e4f39d34207350b03b21e9b937d72d19b8993e1
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-05-25 21:41:15 -0700

    refactor

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -3,9 +3,7 @@ import {Link} from "react-router-dom";
 
 
 const Filter = (props) => {
-    const refreshPage = () => {
-      window.location.reload(false);
-    };
+   
 
     return (
       <section className="filter">
